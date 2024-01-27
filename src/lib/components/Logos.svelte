5ad92d217977dd01c0c284200443973df2806087
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-26 23:42:55 -0600

    Update Logos.svelte

---
+++ src/lib/components/Logos.svelte
@@ -0,0 +1,39 @@
+<div class="flex justify-center items-center mx-auto mb-[30px]">
+	<a href="https://keep.google.com/u/0/" target="_blank" class="w-[40px] mr-2 keep">
+		<img
+			class="hover:scale-125 transform transition duration-200"
+			src="imgs/keep.png"
+			alt="Google Keep"
+		/>
+	</a>
+
+	<div class="plus">+</div>
+
+	<a href="https://svelte.dev/docs/introduction" target="_blank" class="w-[65px] svelte">
+		<img
+			class="hover:scale-125 transform transition duration-200"
+			src="imgs/svelte.png"
+			alt="Svelte"
+		/>
+	</a>
+
+	<div class="plus">+</div>
+
+	<a href="https://console.firebase.google.com/" target="_blank" class="w-[40px] mx-2 firebase">
+		<img
+			class="hover:scale-125 transform transition duration-200"
+			src="imgs/firebase.png"
+			alt="Firebase"
+		/>
+	</a>
+</div>
+
+<style>
+	.plus {
+		@apply mx-2.5 font-normal;
+	}
+
+	img {
+		@apply max-w-full align-middle;
+	}
+</style>
