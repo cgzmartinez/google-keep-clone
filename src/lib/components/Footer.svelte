4f4c75a175578d575f06cbbf74b8d9926e90dc24
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-26 23:42:46 -0600

    Create Footer.svelte

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,5 @@
+<div class="flex justify-center items-center absolute inset-x-0 bottom-0 mb-10">
+	<p class="text-gray-400">Styled with</p>
+
+	<img src="./imgs/tailwind.png" class="ml-2 w-auto h-5" alt="" />
+</div>
