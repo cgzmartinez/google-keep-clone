4e389eeac25252a0d1b75b934e027ffd0b8c081b
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-27 23:07:27 -0600

    Update Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,10 @@
 <div class="flex justify-center items-center absolute inset-x-0 bottom-0 mb-10">
 	<p class="text-gray-400">Styled with</p>
 	<a href="https://tailwindcss.com/" target="_blank">
-		<img src="./imgs/tailwind.png" class="ml-2 w-auto h-5" alt="" />
+		<img
+			src="./imgs/tailwind.png"
+			class="ml-2 w-auto h-5 hover:scale-125 transform transition duration-200"
+			alt=""
+		/>
 	</a>
 </div>
