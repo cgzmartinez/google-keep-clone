19668369ce5ad3e814a07fdc456bd461069148d2
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-27 23:05:49 -0600

    Update Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,6 @@
 <div class="flex justify-center items-center absolute inset-x-0 bottom-0 mb-10">
 	<p class="text-gray-400">Styled with</p>
-
-	<img src="./imgs/tailwind.png" class="ml-2 w-auto h-5" alt="" />
+	<a href="https://tailwindcss.com/" target="_blank">
+		<img src="./imgs/tailwind.png" class="ml-2 w-auto h-5" alt="" />
+	</a>
 </div>
